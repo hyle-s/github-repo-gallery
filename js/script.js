264ce32e472dbe70663346fbe988c08c1e21736d
hyle_s <samhyle@gmail.com>
2023-10-17 00:52:37 -0500

    Added functions to fetch and display repo data

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 const overview = document.querySelector(".overview");
 const username = "hyle-s";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
-  const userInfo = await fetch(`https://api.github.com/users/${username}`);
-  const data = await userInfo.json();
-  displayUserInfo(data);
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    displayUserInfo(data);
 };
 
 gitUserInfo();
 
 const displayUserInfo = function (data) {
-  const div = document.createElement("div");
-  div.classList.add("user-info");
-  div.innerHTML = `
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
     <figure>
       <img alt="user avatar" src=${data.avatar_url} />
     </figure>
@@ -23,5 +24,22 @@ const displayUserInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>
   `;
-  overview.append(div);
+    overview.append(div);
+    gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`)
+    const repoData = await fetchRepos.json();
+    console.log(repoData);
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
